df7794aa52ed4066e63713d96a7a883f692be284
kgrzymski <k.grzymski@mobilinktech.pl>
2017-12-20 14:21:14 +0100

    modified

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -9,8 +9,8 @@ var appScriptsDir = process.env.IONIC_APP_SCRIPTS_DIR;
 var config = require(path.join(appScriptsDir, 'config', 'webpack.config.js'));
 
 var env = process.env.IONIC_ENV ? process.env.IONIC_ENV : 'dev';
-console.log(chalk.green(`PROJECT: [`, chalk.underline.yellow(`${env}`), `] mode`));
-console.log(chalk.green(`PROJECT: [`, chalk.underline.yellow(`${projectRootDir}`), `] dir`));
+console.log(chalk.green(`ENV: [`, chalk.underline.yellow(`${env}`), `]`));
+console.log(chalk.green(`DIR: [`, chalk.underline.yellow(`${projectRootDir}`), `]`));
 
 var envVars;
 try {
